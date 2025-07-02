100ade05d79d40b4e519661aede35d3b559f8cee
Thainá Mariano <thaina.mariano@alunos.unis.edu.br>
2025-07-02 20:05:05 -0300

    Update Chapéu-seletor.php

--- Harry Potter - PHP POO/src/Model/Chapéu-seletor.php
+++ Harry Potter - PHP POO/src/Model/Chapéu-seletor.php
@@ -1,35 +1,19 @@
 <?php
+declare(strict_types=1);
+
+namespace App\Model;
 
 class ChapeuSeletor {
-    public $casas = ['Grifinória', 'Sonserina', 'Corvinal', 'Lufa-Lufa'];
+    public array $casas = ['Grifinória', 'Sonserina', 'Corvinal', 'Lufa-Lufa'];
 
-
-    public function selecionarCasa($aluno) {
+    public function selecionarCasa(string $aluno): void {
         $casa = $this->casas[array_rand($this->casas)];
         echo "Aluno(a) {$aluno} foi selecionado(a) para a casa: <b>{$casa}</b><br>";
     }
 }
 
-function enviarEmail($alunos, $dumbledore, &$resposta) {
-
+function enviarEmail(array $alunos, string $dumbledore, string &$resposta): bool {
+    // Simulação de envio de email
     $resposta = "Email enviado com sucesso!";
     return true;
 }
-
-
-$alunosAceitos = ['Harry Potter', 'Hermione Granger', 'Ron Weasley'];
-$dumbledore = 'Alvo Dumbledore';
-$resposta = '';
-
-if (enviarEmail($alunosAceitos, $dumbledore, $resposta)) {
-    echo "<h2>A cerimônia de seleção vai começar...</h2><br>";
-
-    $chapeu = new ChapeuSeletor();
-
-    foreach ($alunosAceitos as $aluno) {
-        $chapeu->selecionarCasa($aluno);
-    }
-} else {
-    echo "Falha ao enviar email: {$resposta}";
-}
-
